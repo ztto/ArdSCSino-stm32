5506a900422f55fa5b1932d52f24f31acb8ad517
Eric Helgeson <erichelgeson@gmail.com>
2022-12-22 08:55:46 -0600

    Back to snapshot

--- src/BlueSCSI.cpp
+++ src/BlueSCSI.cpp
@@ -44,7 +44,7 @@
                                 // 2: Debug information output to LOG.txt (slow)
 
 // Log File
-#define VERSION "1.1-20221203"
+#define VERSION "1.1-20221222-SNAPSHOT"
 #define LOG_FILENAME "LOG.txt"
 
 #include "BlueSCSI.h"
