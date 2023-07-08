e0d115f33c22869c6303712b1290573802431113
Eric Helgeson <erichelgeson@gmail.com>
2023-07-08 11:53:38 -0500

    v1.1-20230708

--- src/BlueSCSI.cpp
+++ src/BlueSCSI.cpp
@@ -44,7 +44,7 @@
                                 // 2: Debug information output to LOG.txt (slow)
 
 // Log File
-#define VERSION "1.1-20221222-SNAPSHOT"
+#define VERSION "1.1-20230708"
 #define LOG_FILENAME "LOG.txt"
 
 #include "BlueSCSI.h"
